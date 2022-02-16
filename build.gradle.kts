f3a721b74489c840683faf63ccf39ea69736385b
LaoLittle <y1941803875@outlook.com>
2022-02-16 11:26:31 +0800

    fix: implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "org.laolittle.plugin.draw"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,9 @@ repositories {
 fun skikoAwt(ver: String) = "org.jetbrains.skiko:skiko-awt-runtime-$ver"
 
 dependencies {
-    val skikoVer = "0.7.5"
+    val skikoVer = "0.7.8"
     implementation(skikoAwt("windows-x64:$skikoVer"))
+    implementation(skikoAwt("linux-x64:$skikoVer"))
+    implementation(skikoAwt("linux-arm64:$skikoVer"))
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
