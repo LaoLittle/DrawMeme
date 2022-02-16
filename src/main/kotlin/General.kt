a3bff095d5d5986ce3209a229a99dd254be518bc
LaoLittle <y1941803875@outlook.com>
2022-02-16 11:26:56 +0800

    fix: trim

--- src/main/kotlin/General.kt
+++ src/main/kotlin/General.kt
@@ -5,16 +5,15 @@ internal fun String.split(): List<String>? {
         when {
             this.length == 1 -> listOf(this, " ")
             contains("\n") -> split(Regex("\n+"))
-            else -> split(Regex("[\\s　]+"))
+            else -> trim().split(Regex("[\\s　]+"))
         }.toMutableList()
 
     if (words.isEmpty()) return null
     if (words.size == 1) {
         words.apply {
-            val sentence = words[0]
+            val sentence = words[0].also { if (it.isBlank()) return null }
             clear()
-            val left = if (sentence.isNotEmpty())
-                (sentence.length shr 1) else 1
+            val left = sentence.length shr 1
             add(sentence.substring(0, left))
             if (sentence.length == 1)
                 add(" ")
